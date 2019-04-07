d4b95a109b7c7a2e6ae3f0528e0df87cc6ea19ae
Shivani Gupta <shivanigupta2608@gmail.com>
2019-04-06 20:03:08 -0400

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,123 +1,128 @@
-# Install needed packages:
-#
-#install.packages("data.table")
-#install.packages("reshape2")
-#install.packages("knitr")
+# run_analysis.R should do the following:
+#   
+#   1)  Merges the training and the test sets to create one data set.
+# 
+#   2)  Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 
+#   3)  Uses descriptive activity names to name the activities in the data set
+# 
+#   4)  Appropriately labels the data set with descriptive variable names. 
+#   
+#   5)  From the data set in step 4, creates a second, independent tidy data set with the average
+#       of each variable for each activity and each subject.
 
-library(data.table)
-library("reshape2")
-library("knitr")
+#     Please upload the tidy data set created in step 5 of the instructions. 
 
-# Change to project directory:
-setwd("D:/DATA/GettingCleaningData/Project")
+#     Please upload your data set as a txt file created with write.table() using row.name=FALSE 
+#     (do not cut and paste a dataset directly into the text box, as this may cause errors 
+#     saving your submission).
 
-##################################################################################
-# 1. Merges the training and the test sets to create one data set.
+# Load the needed packages
+packages <- c("data.table", "reshape2", "dplyr")
+sapply(packages, require, character.only=TRUE, quietly=TRUE)
 
-# Read in subject training/test data:
-dtTrain <- fread("Dataset/train/subject_train.txt")
-dtTest  <- fread("Dataset/test/subject_test.txt" )
+# Assumes the Git repository : https://github.com/dholtz/GettingAndCleaningData
+# has been cloned to a users local machine, and the R, setwd(), has been used 
+# to set the working directory to the root of this cloned repository.
+path <- getwd()
 
-# Read in activity training/test data:
-
-# Helper function to read in large data sets correctly:
-readtable <- function (file) {
-  dataf <- read.table(file)
-  dt <- data.table(dataf)
-  
-  return(dt)
+# Give warning to set the working directory if not able to find data files.
+projectDataPath <- file.path(path, "project_data")
+fileCount <- length(list.files(projectDataPath, recursive=TRUE))
+if (fileCount != 28) {
+  stop("Please use setwd() to the root of the cloned repository.")
 }
 
-dtATrain <- readtable("Dataset/train/Y_train.txt")
-dtATest <- readtable("Dataset/test/Y_test.txt")
-dtTrain <- readtable("Dataset/train/X_train.txt")
-dtTest <- readtable("Dataset/test/X_test.txt")
+# Read in the 'Subject' data
+dtTrainingSubjects <- fread(file.path(projectDataPath, "train", "subject_train.txt"))
+dtTestSubjects  <- fread(file.path(projectDataPath, "test" , "subject_test.txt" ))
 
-# Merge training and test datasets::
-dtSubject <- rbind(dtTrain, dtTest)
-dtActivity <- rbind(dtATrain, dtATest)
+# Read in the 'Activity' data
+dtTrainingActivity <- fread(file.path(projectDataPath, "train", "Y_train.txt"))
+dtTestActivity  <- fread(file.path(projectDataPath, "test" , "Y_test.txt" ))
 
-# Set column names:
-setnames(dtSubject, "V1", "subject")
-setnames(dtActivity, "V1", "activityNum")
+# Read in the 'Measurements' data
+# Switching to standard, read.table to avoid the following possible error:
+# https://github.com/Rdatatable/data.table/issues/487
+# No time to figure out where this, 'works again now' version is
+dtTrainingMeasures <- data.table(read.table(file.path(projectDataPath, "train", "X_train.txt")))
+dtTestMeasures  <- data.table(read.table(file.path(projectDataPath, "test" , "X_test.txt")))
 
-dt <- rbind(dtTrain, dtTest)
+# Row merge the Training and Test Subjects
+# http://www.statmethods.net/management/merging.html
+dtSubjects <- rbind(dtTrainingSubjects, dtTestSubjects)
+# Why setnames() ?? http://stackoverflow.com/questions/10655438/rename-one-named-column-in-r
+setnames(dtSubjects, "V1", "subject")
 
-dtSubject <- cbind(dtSubject, dtActivity)
-dt <- cbind(dtSubject, dt)
+# Row merge the Training and Test Activities
+dtActivities <- rbind(dtTrainingActivity, dtTestActivity)
+setnames(dtActivities, "V1", "activityNumber")
 
-setkey(dt, subject, activityNum)
+# Merge the Training and Test 'Measurements' data
+dtMeasures <- rbind(dtTrainingMeasures, dtTestMeasures)
 
-##################################################################################
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# Column merge the subjects to activities
+dtSubjectActivities <- cbind(dtSubjects, dtActivities)
+dtSubjectAtvitiesWithMeasures <- cbind(dtSubjectActivities, dtMeasures)
 
-dtFeatures <- fread("Dataset/features.txt")
-setnames(dtFeatures, names(dtFeatures), c("featureNum", "featureName"))
+# Order all of the combined data by, subject and activity
+setkey(dtSubjectAtvitiesWithMeasures, subject, activityNumber)
 
-dtFeatures <- dtFeatures[grepl("mean\\(\\)|std\\(\\)", featureName)]
+## Read in the 'features.txt' 
+## This file matches up to the columns in the data.table, dtSubjectActivitiesWithMeasures
+## with the features/measures.
+dtAllFeatures <- fread(file.path(projectDataPath, "features.txt"))
+setnames(dtAllFeatures, c("V1", "V2"), c("measureNumber", "measureName"))
 
-dtFeatures$featureCode <- dtFeatures[, paste0("V", featureNum)]
-head(dtFeatures)
-dtFeatures$featureCode
+# Use grepl to just get features/measures related to mean and std
+dtMeanStdMeasures <- dtAllFeatures[grepl("(mean|std)\\(\\)", measureName)]
+# Create a column to 'index/cross reference' into the 'measure' headers
+# in dtSubjectActivitiesWithMeasures
+dtMeanStdMeasures$measureCode <- dtMeanStdMeasures[, paste0("V", measureNumber)]
 
-select <- c(key(dt), dtFeatures$featureCode)
-dt <- dt[, select, with=FALSE]
+# Build up the columns to select from the data.table,
+# dtSubjectActivitiesWithMeasures
+columnsToSelect <- c(key(dtSubjectAtvitiesWithMeasures), dtMeanStdMeasures$measureCode)
+# Just take the rows with the columns of interest ( std() and mean() )
+dtSubjectActivitesWithMeasuresMeanStd <- subset(dtSubjectAtvitiesWithMeasures, 
+                                                select = columnsToSelect)
 
-##################################################################################
-# 3. Uses descriptive activity names to name the activities in the data set
+# Read in the activity names and give them more meaningful names
+dtActivityNames <- fread(file.path(projectDataPath, "activity_labels.txt"))
+setnames(dtActivityNames, c("V1", "V2"), c("activityNumber", "activityName"))
 
-dtActivityNames <- fread("Dataset/activity_labels.txt")
-setnames(dtActivityNames, names(dtActivityNames), c("activityNum", "activityName"))
+# Merge the 'meaningful activity names' with the 
+# dtSubjectActiitiesWithMeasuresMeanStd
+dtSubjectActivitesWithMeasuresMeanStd <- merge(dtSubjectActivitesWithMeasuresMeanStd, 
+                                               dtActivityNames, by = "activityNumber", 
+                                               all.x = TRUE)
 
-dt <- merge(dt, dtActivityNames, by="activityNum", all.x=TRUE)
+# Sort the data.table, dtSubjectActivitesWithMeasuresMeanStd
+setkey(dtSubjectActivitesWithMeasuresMeanStd, subject, activityNumber, activityName)
 
-##################################################################################
-# 4. Appropriately labels the data set with descriptive variable names. 
+# Convert from a wide to narrow data.table using the keys created earlier
+dtSubjectActivitesWithMeasuresMeanStd <- data.table(melt(dtSubjectActivitesWithMeasuresMeanStd, 
+                                                         id=c("subject", "activityName"), 
+                                                         measure.vars = c(3:68), 
+                                                         variable.name = "measureCode", 
+                                                         value.name="measureValue"))
 
-setkey(dt, subject, activityNum, activityName)
+# Merge measure codes
+dtSubjectActivitesWithMeasuresMeanStd <- merge(dtSubjectActivitesWithMeasuresMeanStd, 
+                                               dtMeanStdMeasures[, list(measureNumber, measureCode, measureName)], 
+                                               by="measureCode", all.x=TRUE)
 
-##################################################################################
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+# Convert activityName and measureName to factors
+dtSubjectActivitesWithMeasuresMeanStd$activityName <- 
+  factor(dtSubjectActivitesWithMeasuresMeanStd$activityName)
+dtSubjectActivitesWithMeasuresMeanStd$measureName <- 
+  factor(dtSubjectActivitesWithMeasuresMeanStd$measureName)
 
-dt <- data.table(melt(dt, key(dt), variable.name="featureCode"))
+# Reshape the data to get the averages 
+measureAvgerages <- dcast(dtSubjectActivitesWithMeasuresMeanStd, 
+                          subject + activityName ~ measureName, 
+                          mean, 
+                          value.var="measureValue")
 
-dt <- merge(dt, dtFeatures[, list(featureNum, featureCode, featureName)], by="featureCode", all.x=TRUE)
-
-dt$activity <- factor(dt$activityName)
-dt$feature <- factor(dt$featureName)
-
-grep_feature <- function (regex) {
-  grepl(regex, dt$feature)
-}
-
-## Features with 2 categories
-n <- 2
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grep_feature("^t"), grep_feature("^f")), ncol=nrow(y))
-dt$featDomain <- factor(x %*% y, labels=c("Time", "Freq"))
-x <- matrix(c(grep_feature("Acc"), grep_feature("Gyro")), ncol=nrow(y))
-dt$featInstrument <- factor(x %*% y, labels=c("Accelerometer", "Gyroscope"))
-x <- matrix(c(grep_feature("BodyAcc"), grep_feature("GravityAcc")), ncol=nrow(y))
-dt$featAcceleration <- factor(x %*% y, labels=c(NA, "Body", "Gravity"))
-x <- matrix(c(grep_feature("mean()"), grep_feature("std()")), ncol=nrow(y))
-dt$featVariable <- factor(x %*% y, labels=c("Mean", "SD"))
-## Features with 1 category
-dt$featJerk <- factor(grep_feature("Jerk"), labels=c(NA, "Jerk"))
-dt$featMagnitude <- factor(grep_feature("Mag"), labels=c(NA, "Magnitude"))
-## Features with 3 categories
-n <- 3
-y <- matrix(seq(1, n), nrow=n)
-x <- matrix(c(grep_feature("-X"), grep_feature("-Y"), grep_feature("-Z")), ncol=nrow(y))
-dt$featAxis <- factor(x %*% y, labels=c(NA, "X", "Y", "Z"))
-
-r1 <- nrow(dt[, .N, by=c("feature")])
-r2 <- nrow(dt[, .N, by=c("featDomain", "featAcceleration", "featInstrument", "featJerk", "featMagnitude", "featVariable", "featAxis")])
-r1 == r2
-
-setkey(dt, subject, activity, featDomain, featAcceleration, featInstrument, featJerk, featMagnitude, featVariable, featAxis)
-dtTidy <- dt[, list(count = .N, average = mean(value)), by=key(dt)]
-
-##################################################################################
-# Please upload the tidy data set created in step 5 of the instructions. Please upload your data set as a txt file created with write.table() using row.name=FALSE (do not cut and paste a dataset directly into the text box, as this may cause errors saving your submission).
-
-write.table(dtTidy, "tidy.data.txt", row.name=FALSE)
+# Write the tab delimited file
+write.table(measureAvgerages, file="tidyData.txt", row.name=FALSE, sep = "\t")
